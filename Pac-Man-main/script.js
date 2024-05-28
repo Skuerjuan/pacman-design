ede91228cf4f4ef4080d150bece0cc712dad938a
Skuerjuan <maierowiczfelipe@gmail.com>
2024-05-28 14:13:43 -0300

    Funcionalidad añadida, CSS mejorado

---
+++ Pac-Man-main/script.js
@@ -0,0 +1,6 @@
+function rickroll(lol){
+    if(lol == "rick")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank');
+    if(lol == "pancho")
+    window.open("https://es.wikipedia.org/wiki/Hot_dog", '_blank');
+}
